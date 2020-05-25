9bce0b6bcdf9efc54c8052bc615059eb16dbe922
sylwek <githubsylop@gmail.com>
2020-05-25 22:19:28 +0200

    examplee

--- chapter1.js
+++ chapter1.js
@@ -10,3 +10,9 @@ var s = '1s'; s++;
 console.log(s)
 var test= 10 % 0;
 console.log(test)
+var test2 = "1"
+test2= test2 + 3
+var  test3 = test2 + 13
+var  test4 = test2 / 13
+console.log(test3)
+console.log(test4)
