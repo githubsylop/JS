b5c617c1d57f7113bdf83da1258b4609af52ee07
sylwek <githubsylop@gmail.com>
2020-05-25 22:14:30 +0200

    next

--- chapter1.js
+++ chapter1.js
@@ -1,5 +1,12 @@
 var infinity = 1e329;
 var fault = 1/0;
 fault
-console.log(typeof fault);
+console.log( fault);
 console.log('test');
+var v = v || 10;
+console.log(v)
+var d = !!"false";
+var s = '1s'; s++;
+console.log(s)
+var test= 10 % 0;
+console.log(test)
